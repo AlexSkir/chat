cfcfcddc2fb30c92acbfb28f10e11282792af29a
AlexSkir <a.skirnevskaia@gmail.com>
2019-10-12 23:35:39 +0300

    build: create  production build

---
+++ build/precache-manifest.6b499a2e279ed50e5653867d794dafdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5564f213eaa1153926072444167bbbfd",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "4d967bf5147f8f85eb34",
+    "url": "/chat/static/css/2.acfe43d3.chunk.css"
+  },
+  {
+    "revision": "34c649439a556ec37dc7",
+    "url": "/chat/static/css/main.ce6dc3a7.chunk.css"
+  },
+  {
+    "revision": "4d967bf5147f8f85eb34",
+    "url": "/chat/static/js/2.0a2bb62c.chunk.js"
+  },
+  {
+    "revision": "34c649439a556ec37dc7",
+    "url": "/chat/static/js/main.b65b3275.chunk.js"
+  },
+  {
+    "revision": "5bb637843dac5d879ad5",
+    "url": "/chat/static/js/runtime~main.54cd968c.js"
+  },
+  {
+    "revision": "baad24afb268fef8ea40f2eb3fb6f24e",
+    "url": "/chat/static/media/BG.baad24af.png"
+  },
+  {
+    "revision": "1362051084b74874e8fb581ddbf746ff",
+    "url": "/chat/static/media/sound.13620510.mp3"
+  }
+]);
