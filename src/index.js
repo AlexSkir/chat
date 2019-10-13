b7cb5d799d57e82f342885117bfeb5597418fd80
AlexSkir <a.skirnevskaia@gmail.com>
2019-10-13 03:04:24 +0300

    style: fix mobile layout

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import 'index.css';
 import App from 'app';
 import store from 'store/store';
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
