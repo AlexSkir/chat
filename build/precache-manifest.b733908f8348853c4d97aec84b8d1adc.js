5509537ede8a142386c9972c2b99f0efc6e5a7f0
AlexSkir <a.skirnevskaia@gmail.com>
2020-05-07 03:03:27 +0300

    add build version

---
+++ build/precache-manifest.b733908f8348853c4d97aec84b8d1adc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1244643e0b311cd1eb737b64f5bba817",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "40c36de83d4ea83501a8",
+    "url": "/chat/static/css/2.8ca7cecf.chunk.css"
+  },
+  {
+    "revision": "6fb884e9cf0dae35d76e",
+    "url": "/chat/static/css/main.c2fc931b.chunk.css"
+  },
+  {
+    "revision": "40c36de83d4ea83501a8",
+    "url": "/chat/static/js/2.50d987a3.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/chat/static/js/2.50d987a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb884e9cf0dae35d76e",
+    "url": "/chat/static/js/main.1175119b.chunk.js"
+  },
+  {
+    "revision": "e94c8164bca87db4a7ad",
+    "url": "/chat/static/js/runtime-main.4fdc9441.js"
+  },
+  {
+    "revision": "baad24afb268fef8ea40f2eb3fb6f24e",
+    "url": "/chat/static/media/BG.baad24af.png"
+  },
+  {
+    "revision": "1362051084b74874e8fb581ddbf746ff",
+    "url": "/chat/static/media/sound.13620510.mp3"
+  }
+]);
