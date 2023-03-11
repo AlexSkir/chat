c1ecc93b776657330120dd98ebe1315432eedd93
AlexSkir <a.skirnevskaia@gmail.com>
2023-03-11 20:24:47 +0300

    build production

---
+++ build/precache-manifest.82c17fa302727afff969715848347bd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf266ea618e0999ded9b4fd08812d8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a987e6cfb33e9cb3f78",
+    "url": "/static/css/2.73a3b7f4.chunk.css"
+  },
+  {
+    "revision": "3e15f262b09fda13ce11",
+    "url": "/static/css/main.bef1abf4.chunk.css"
+  },
+  {
+    "revision": "5a987e6cfb33e9cb3f78",
+    "url": "/static/js/2.85861c9e.chunk.js"
+  },
+  {
+    "revision": "3e15f262b09fda13ce11",
+    "url": "/static/js/main.6c56abe7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "baad24afb268fef8ea40f2eb3fb6f24e",
+    "url": "/static/media/BG.baad24af.png"
+  },
+  {
+    "revision": "1362051084b74874e8fb581ddbf746ff",
+    "url": "/static/media/sound.13620510.mp3"
+  }
+]);
